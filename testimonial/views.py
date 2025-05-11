5e8afae13ced28216258a5cf88df586356b81c59
loveshsingh <loveshsinghsuperfine@gmail.com>
2025-05-11 20:55:50 +0530

    Fix: Testimonial pagination

--- testimonial/views.py
+++ testimonial/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Testimonial
+from django.core.paginator import Paginator
 
 def testimonials_page(request):
     testimonials = Testimonial.objects.all()
-    return render(request, 'testimonial/testimonials.html', {'testimonials': testimonials})
+
+    # paginate: 5 per page
+    paginator = Paginator(testimonials, 6)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'testimonial/testimonials.html', {'testimonials': page_obj})
